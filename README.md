c0c931c67f322b02692b0cb4bf411313b0ccfd0d
Mocha Dcaf <mocha@dcaf.so>
2022-07-18 19:03:02 -0400

    token_swap -> tokenswap

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 # OR
 anchor-go --src=../drip-program/target/idl/drip.json --dst=./pkg/drip
 anchor-go --src=./idls/whirlpool.json --dst=./pkg/whirlpool
-anchor-go --src=./idls/token_swap.json --codec=bin --dst=./pkg/tokenswap
+anchor-go --src=./idls/tokenswap.json --codec=bin --dst=./pkg/tokenswap
 ```
--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 anchor-go --src=../drip-program/target/idl/drip.json --dst=./pkg/drip
 anchor-go --src=./idls/whirlpool.json --dst=./pkg/whirlpool
-anchor-go --src=./idls/token_swap.json --codec=bin --dst=./pkg/tokenswap
+anchor-go --src=./idls/tokenswap.json --codec=bin --dst=./pkg/tokenswap
