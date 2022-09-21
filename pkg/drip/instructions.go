da320fd976a499837fc5c3111c751171dc5280ab
Mocha Dcaf <mocha@dcaf.so>
2022-09-20 20:16:36 -0400

    chore: update solana-go-clients

--- pkg/drip/instructions.go
+++ pkg/drip/instructions.go
@@ -34,11 +34,11 @@ var (
 
 	Instruction_Deposit = ag_binary.TypeID([8]byte{242, 35, 198, 137, 82, 225, 242, 182})
 
+	Instruction_DepositWithMetadata = ag_binary.TypeID([8]byte{66, 112, 168, 108, 67, 61, 27, 151})
+
 	Instruction_WithdrawB = ag_binary.TypeID([8]byte{28, 146, 254, 247, 183, 161, 195, 149})
 
 	Instruction_ClosePosition = ag_binary.TypeID([8]byte{123, 134, 81, 0, 49, 68, 98, 98})
-
-	Instruction_DepositWithMetadata = ag_binary.TypeID([8]byte{66, 112, 168, 108, 67, 61, 27, 151})
 
 	Instruction_DripSplTokenSwap = ag_binary.TypeID([8]byte{129, 32, 61, 181, 42, 74, 219, 106})
 
@@ -58,12 +58,12 @@ func InstructionIDToName(id ag_binary.TypeID) string {
 		return "InitVaultPeriod"
 	case Instruction_Deposit:
 		return "Deposit"
+	case Instruction_DepositWithMetadata:
+		return "DepositWithMetadata"
 	case Instruction_WithdrawB:
 		return "WithdrawB"
 	case Instruction_ClosePosition:
 		return "ClosePosition"
-	case Instruction_DepositWithMetadata:
-		return "DepositWithMetadata"
 	case Instruction_DripSplTokenSwap:
 		return "DripSplTokenSwap"
 	case Instruction_DripOrcaWhirlpool:
@@ -102,13 +102,13 @@ var InstructionImplDef = ag_binary.NewVariantDefinition(
 			"deposit", (*Deposit)(nil),
 		},
 		{
+			"deposit_with_metadata", (*DepositWithMetadata)(nil),
+		},
+		{
 			"withdraw_b", (*WithdrawB)(nil),
 		},
 		{
 			"close_position", (*ClosePosition)(nil),
-		},
-		{
-			"deposit_with_metadata", (*DepositWithMetadata)(nil),
 		},
 		{
 			"drip_spl_token_swap", (*DripSplTokenSwap)(nil),
